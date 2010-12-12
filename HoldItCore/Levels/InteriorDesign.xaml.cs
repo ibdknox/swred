c17c26fdc1e408b4f8d081280b05b430240a1269
Pete Blois <github@blois.us>
2010-12-11 20:20:54 -0800

    Added delay for how long until pee starts to build up.

---
+++ HoldItCore/Levels/InteriorDesign.xaml.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Windows.Threading;
+using System;
+using HoldItCore.People;
+
+namespace HoldItCore.Levels {
+	public partial class InteriorDesign : Level {
+
+		DispatcherTimer timer = new DispatcherTimer() {
+			Interval = TimeSpan.FromSeconds(3),
+		};
+
+		private Random rng = new Random();
+
+		public InteriorDesign() {
+			InitializeComponent();
+
+			this.timer.Tick += this.HandleTick;
+		}
+
+		protected override void Start() {
+			base.Start();
+
+			this.timer.Start();
+
+			this.AddPerson(new Person());
+		}
+
+		protected override void Stop() {
+			base.Stop();
+
+			this.timer.Stop();
+		}
+
+		private void HandleTick(object sender, EventArgs e) {
+			double val = rng.NextDouble();
+			if (val <= .5)
+				this.AddPerson(new OldMan());
+			else
+				this.AddPerson(new Person());
+		}
+	}
+}
